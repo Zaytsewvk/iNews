3962a61801b9aea4dbedbde8af9d299cf0a104b7
Vadim <zaytsewvk@gmail.com>
2019-03-25 16:49:58 +0200

    Updates

---
+++ precache-manifest.dc8af139585eee1c630530a887bda1ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "044388472daaaf6d083a",
+    "url": "/inews/static/css/main.d621054f.chunk.css"
+  },
+  {
+    "revision": "044388472daaaf6d083a",
+    "url": "/inews/static/js/main.db3589bd.chunk.js"
+  },
+  {
+    "revision": "8ba6abf2688deedf8c53",
+    "url": "/inews/static/js/runtime~main.f0c6ae26.js"
+  },
+  {
+    "revision": "2c5a3201bb2139efbfd1",
+    "url": "/inews/static/js/2.0438198b.chunk.js"
+  },
+  {
+    "revision": "226e26460be5c4fcebc4acd1e6a85fd7",
+    "url": "/inews/static/media/logo.226e2646.svg"
+  },
+  {
+    "revision": "5148362073a71fe685023b441060b627",
+    "url": "/inews/index.html"
+  }
+];
